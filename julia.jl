9940acdf7dd0200ff68931bd515827ea60251d8e
Harmen Stoppels <harmenstoppels@gmail.com>
2021-04-29 19:20:59 +0200

    Add l3 cache

--- julia.jl
+++ julia.jl
@@ -1,5 +1,6 @@
 using LoopVectorization
 using LoopVectorization: StaticInt
+using VectorizationBase: cache_size
 using Libdl
 using Plots
 using Statistics: median
@@ -12,8 +13,9 @@ using Random: shuffle
 
 const lib = Libdl.dlopen("./libxsmm_bench_f64.so")
 const sym = Libdl.dlsym(lib, :bench_f64)
+const l3 = cache_size(Val(3))
 
-bench_libxsmm!(C, A, B, repetitions=10) = ccall(sym, Cdouble, 
+bench_libxsmm!(C, A, B, repetitions) = ccall(sym, Cdouble, 
     (Ptr{Float64}, Ptr{Float64}, Ptr{Float64}, Cint, Cint, Cint, Cint, Cint),
     C, A, B, size(C, 1), size(C, 2), size(A, 2), size(A, 3), repetitions)
 
@@ -40,20 +42,22 @@ function lv!(C, A, B, ::Val{ms}, ::Val{ns}, ::Val{ks}) where {ms,ns,ks}
     C
 end
 
-function benchmark(ms=1:32, ns=1:32, ks=1:32, b=10_000, repetitions=10, dir=joinpath(@__DIR__, "assets", Sys.CPU_NAME))
+function benchmark(; ms=1:32, ns=1:32, ks=1:32, dir=joinpath(@__DIR__, "assets", Sys.CPU_NAME), repetitions = 25, L3_cache::StaticInt{L3} = l3) where {L3}
     results = [(0.0, 0.0) for m in ms, n in ns, k in ks]
 
-    i = 0
+    i = 1
 
     for (mi, m) in enumerate(ms), (ni, n) in enumerate(ns), (ki, k) in enumerate(ks)
         @show (m, n, k)
+
+        batchsize = L3 ÷ (sizeof(Float64) * (m * k + k * n + m * n))
 
         if i % 500 == 0
             save_to_hdf5(results, ms, ns, ks, dir)
         end
 
-        A = rand(m, k, b)
-        B = rand(k, n, b)
+        A = rand(m, k, batchsize)
+        B = rand(k, n, batchsize)
 
         C_libxsmm    = zeros(m, n)
         C_lv         = zeros(m, n)
@@ -65,7 +69,7 @@ function benchmark(ms=1:32, ns=1:32, ks=1:32, b=10_000, repetitions=10, dir=join
 
         err = maximum(abs, C_lv - C_libxsmm)
 
-        if err > 200 * k * b * eps(Float64)
+        if err > 200 * k * batchsize * eps(Float64)
             println("Large error at for: (", m, ",", n, ",", k, ") = ", err)
         end
 
